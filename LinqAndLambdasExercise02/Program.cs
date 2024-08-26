12415909d2694189a4c5a7912f0885599d2560c8
LukSt92 <lukasz.m.stasiak@gmail.com>
2024-08-26 14:02:54 +0200

    Add: new class Add: main method

--- LinqAndLambdasExercise02/Program.cs
+++ LinqAndLambdasExercise02/Program.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace LinqAndLambdasExercise02
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            var listOfObjects = new List<PrintWhenGetting>();
+            for (int i = 1; i < 5; i++)
+                listOfObjects.Add(new PrintWhenGetting() { InstanceNumber = i });
+            Console.WriteLine("Set up the query");
+            var result =
+                from o in listOfObjects
+                select o.InstanceNumber;
+            var immediate = result.ToList();
+            Console.WriteLine("Run the foreach");
+            foreach (var number in immediate)
+                Console.WriteLine($"Writing #{number}");
         }
     }
 }
